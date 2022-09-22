0149fb4aa05e41af40a4f906ba0e9091d62b4fea
FelipeAlfaya <felipealfaya90@gmail.com>
2022-09-22 11:15:16 -0300

    second commit

--- main.go
+++ main.go
@@ -1,5 +1,33 @@
 package main
 
+import "github.com/labstack/echo/v4"
+
+type Car struct {
+	Name  string
+	Price float64
+}
+
+var cars []Car
+
+func CreateCars() {
+	cars = []Car{
+		{Name: "Ford", Price: 19.99},
+		{Name: "BMW", Price: 19.99},
+		{Name: "Chevrolet", Price: 19.99},
+	}
+
+	// 	cars = append(cars, Car{Name: "Ford", Price: 19.99})
+	// 	cars = append(cars, Car{Name: "BMW", Price: 19.99})
+	// 	cars = append(cars, Car{Name: "Chevrolet", Price: 19.99})
+}
+
 func main() {
-	println("Hello", "world!")
+	CreateCars()
+	e := echo.New()
+	e.GET("/cars", getCars)
+	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func getCars(c echo.Context) error {
+	return c.JSON(200, cars)
 }
